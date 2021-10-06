e6eae8c0ce4263e299543b919eb8fa79778e2cab
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2021-10-06 11:34:47 +0330

    Update BrokerIPExtractor.py

--- BrokerIPExtractor.py
+++ BrokerIPExtractor.py
@@ -2,8 +2,9 @@ from os import listdir
 from os.path import isfile, join
 import win32com.client
 import re
+import pandas as pd
 
-mainDirPath = #enter the directory's address e.g: r"C:\Users\Ali Sadeghi Aghili\Downloads\TestDit"
+mainDirPath = "D:\BrokersIPs"
 filesName = [fileName for fileName in listdir(mainDirPath) if (not fileName.startswith('~')) and fileName.endswith('.doc')]
 
 
@@ -18,8 +19,10 @@ for fileName in filesName:
     text = re.findall("Expire.*From Inside", text)[0].replace("From Inside", "")
     DestIPs = re.findall('\d{,3}\\.\d{,3}\\.\d{,3}\\.\d{,3}', text)[1::2]
     Ports = re.findall('11\d{3}', text)
-    brokerName = re.findall("Kargozari.*", fileName)[0].replace('Kargozari', "").replace(".doc", "").strip()
+    brokerName = re.sub(r"Port (Openining|Opening) Form.*\d+[ -]+", "", fileName).replace('Kargozari', "").replace(".doc", "").replace(".docx", "").strip()
     brokersIPPorts[brokerName] = {"IPs": DestIPs, "Ports": Ports}
 
 with open(join(mainDirPath ,"brokersIPPorts.txt"), "w") as file:
     print(brokersIPPorts, file = file)
+    
+dataFrame = pd.DataFrame(brokersIPPorts).transpose().to_csv(join(mainDirPath ,"brokersIPPorts.csv"), sep = ",")
